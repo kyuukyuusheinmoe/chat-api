dc4de245c696a6834eb5504694310a187176acf8
kyuukyuusheinmoe <47250920+kyuukyuusheinmoe@users.noreply.github.com>
2024-07-16 14:25:50 +0700

    add jwt modules

---
+++ src/jwt/jwt.module.ts
@@ -0,0 +1,15 @@
+import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { JwtStrategy } from './jwt.strategy';
+import { JwtAuthService } from './jwt.servic';
+
+@Module({
+  imports: [
+    JwtModule.register({
+      secret: 'my-chat-api',
+      signOptions: { expiresIn: '1h' },
+    }),
+  ],
+  providers: [JwtAuthService, JwtStrategy],
+})
+export class JwtAuthModule {}
