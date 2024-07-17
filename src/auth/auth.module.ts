adeea022aaa96080b9a2780e70c03cc52f1d7635
kyuukyuusheinmoe <47250920+kyuukyuusheinmoe@users.noreply.github.com>
2024-07-17 12:45:19 +0700

    remove jwt service

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,14 +2,13 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { JwtAuthModule } from 'src/jwt/jwt.module';
 import { PrismaService } from '../prisma.service';
-import { JwtService } from '@nestjs/jwt';
 import { AuthController } from './auth.controller';
 import { PasswordService } from 'src/password.service';
 
 @Module({
   imports: [JwtAuthModule],
   controllers: [AuthController],
-  providers: [PrismaService, JwtService, AuthService, PasswordService],
+  providers: [PrismaService, AuthService, PasswordService],
   exports: [AuthService],
 })
 export class AuthModule {}
