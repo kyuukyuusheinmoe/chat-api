d47f45d7e9c28e8cd86a263d71511bc0c3234029
kyuukyuusheinmoe <47250920+kyuukyuusheinmoe@users.noreply.github.com>
2024-07-17 14:06:10 +0700

    fix taking authorization header

--- src/middlewares/user.middleware.ts
+++ src/middlewares/user.middleware.ts
@@ -10,19 +10,19 @@ import { JwtAuthService } from 'src/jwt/jwt.servic';
 export class UserMiddleware implements NestMiddleware {
   constructor(private readonly jwtAuthService: JwtAuthService) {}
 
-  use(req: Request, res: Response, next: NextFunction) {
-    const authorizationHeader = req.headers['Authorization'] as string;
-    const token = authorizationHeader.split(' ')[1];
-
+  async use(req: Request, res: Response, next: NextFunction) {
+    const authorizationHeader = req.headers['authorization'] as string; 
     if (!authorizationHeader) {
       return next();
     }
+    const token = authorizationHeader?.split(' ')[1];
 
     if (!token) {
       return next();
     }
     try {
-      const user = this.jwtAuthService.verifyToken(token);
+
+      const user = await this.jwtAuthService.verifyToken(token);
       req['user'] = user;
     } catch (error) {
       throw new UnauthorizedException('Invalid or expired token');
