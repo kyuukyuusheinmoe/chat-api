76538b622a65606c59c0a90d883c7e4625cf00aa
kyuukyuusheinmoe <47250920+kyuukyuusheinmoe@users.noreply.github.com>
2024-07-18 18:33:13 +0700

    add Message service in chat gateway

--- src/chat/chat.module.ts
+++ src/chat/chat.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ClientsModule, Transport } from '@nestjs/microservices';
 import { ConfigService, ConfigModule } from '@nestjs/config';
+import { MessageService } from 'src/message/message.service';
 
 @Module({
   imports: [
@@ -23,5 +24,6 @@ import { ConfigService, ConfigModule } from '@nestjs/config';
       },
     ]),
   ],
+  providers: [MessageService],
 })
 export class ChatModule {}
