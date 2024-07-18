23fad7a276ccc8db180de42130d9fb0908d24086
kyuukyuusheinmoe <47250920+kyuukyuusheinmoe@users.noreply.github.com>
2024-07-18 18:32:54 +0700

    update message event

--- src/chat/chat.gateway.ts
+++ src/chat/chat.gateway.ts
@@ -7,13 +7,26 @@ import {
   WebSocketGateway,
   WebSocketServer,
 } from '@nestjs/websockets';
-import { Server } from 'socket.io';
+import { Observable } from 'rxjs';
+import { WsResponse } from '@nestjs/websockets';
+import { Server, Socket } from 'socket.io';
+import { MessageBody, ConnectedSocket } from '@nestjs/websockets';
+import { from, map } from 'rxjs';
+import { JwtAuthService } from 'src/jwt/jwt.servic';
+import { MessageService } from 'src/message/message.service';
+import { UserDto } from 'src/auth/auth.dto';
+import { MessageDto } from 'src/message/message.dto';
 
 @WebSocketGateway()
 export class ChatGateway
   implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect
 {
-  private readonly logger = new Logger(ChatGateway.name);
+  constructor(
+    private jwtAuthService: JwtAuthService,
+    private messageService: MessageService,
+  ) {}
+  private readonly logger = new Logger();
+  private users = new Map<string, Socket>();
 
   @WebSocketServer() io: Server;
 
@@ -21,27 +34,99 @@ export class ChatGateway
     this.logger.log('Initialized');
   }
 
-  handleConnection(client: any, ...args: any[]) {
+  async handleConnection(client: any, ...args: any[]) {
     const { sockets } = this.io.sockets;
-    this.logger.log(`Client id : ${client.id} connected`);
-    this.logger.debug(`Number of connected Clients : ${sockets.size}`);
+    const user = await this.getUserDataFromClient(client); // You need to implement this method to get userId
+    if (user) {
+      client.join(user.id);
+      this.users.set(`${user.id}`, client.id);
+      // for (const key of this.users.keys()) {
+      //   this.logger.log(
+      //     `xxx user ${key} user Size . ${this.users.size} Socket Client Size . ${sockets.size}`,
+      //   );
+      // }
+    }
+    this.logger.log(`Client id : ${client.id} connected, UserId : ${user?.id}`);
+    this.logger.debug(`Number of connected Clients : ${sockets.size} `);
+    this.logger.log(
+      `xxx user Size . ${this.users.size} Socket Client Size . ${sockets.size} User Value ${this.users.get(`${user?.id}`)}`,
+    );
   }
 
-  handleDisconnect(client: any) {
-    this.logger.log(`Client id: ${client.id} disconnected`);
+  async handleDisconnect(client: any) {
+    const user = await this.getUserDataFromClient(client); // You need to implement this method to get userId
+    if (user?.id) {
+      client.leave(user.id);
+      this.users.delete(`${user.id}`);
+      console.log(`User ${user.id} disconnected, Socket ID: ${client.id}`);
+    }
   }
 
   @SubscribeMessage('message')
-  async handleMessage(client: any, payload: any): Promise<string> {
-    this.logger.log(`Message received from client id: ${client.id}`);
-    this.logger.debug(`Payload`, payload);
-    // Send message to RabbitMQ
-    return 'Message received and queued';
+  async handleMessage(
+    @MessageBody()
+    data: string,
+    @ConnectedSocket() client: Socket,
+  ) {
+    console.log('xxx handleMessage data ', data, typeof data);
+    // client.join(data.groupId);
+    // this.io.to(data.groupId).emit('message', 'a new challenger approaches');
+
+    const [receiverId, content, groupId] = data.split(',');
+
+    const user = await this.getUserDataFromClient(client);
+    this.saveMessage(user, {
+      content,
+      groupId: +groupId,
+    });
+    // this.sendMessageToUser(receiverId, 'my message', content);
+  }
+
+  sendMessageToUser(userId: string, event: string, message: string) {
+    const client = this.users.get(userId);
+    if (client) {
+      this.io.to(userId).emit(event, message);
+    } else {
+      console.log(`User ${userId} not connected`);
+    }
+  }
+
+  async saveMessage(user: UserDto, data: Omit<MessageDto, 'id' | 'senderId'>) {
+    this.messageService.create(user, data);
+  }
+
+  private async getUserDataFromClient(client: Socket): Promise<UserDto> {
+    const userToken = client.handshake.query.user as string;
+    try {
+      const userData = await this.jwtAuthService.verifyToken(userToken);
+      return userData;
+    } catch (error) {
+      return null;
+    }
   }
 
   // Broadcast a message to all clients
   broadcastMessage(message: string): void {
     this.logger.log(`broadcasting Message`);
     this.io.emit('message', message);
+  }
+
+  @SubscribeMessage('group_joined')
+  joinUserToMeeting(
+    @MessageBody() data: { groupId: string },
+    @ConnectedSocket() client: Socket,
+  ) {
+    console.log('xxx joinUserToMeeting ', data);
+    client.join(data.groupId);
+    this.io.to(data.groupId).emit('message', 'a new challenger approaches');
+    // : Observable<WsResponse<any>>
+    // return from(data.meetingId).pipe(
+    //   map((res: any) => {
+    //     return {
+    //       event: 'group_joined',
+    //       data: data.meetingId,
+    //     };
+    //   }),
+    // );
   }
 }
