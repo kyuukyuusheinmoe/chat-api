7f7e64e51f6049a1c97056b7cd129b3c457597e1
kyuukyuusheinmoe <47250920+kyuukyuusheinmoe@users.noreply.github.com>
2024-07-18 10:49:38 +0700

    update middleware route in app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,12 +9,13 @@ import { JwtAuthService } from './jwt/jwt.servic';
 import { JwtModule } from '@nestjs/jwt';
 import { GroupModule } from './group/group.module';
 import { ChatGateway } from './chat/chat.gateway';
+import { MessageModule } from './message/message.module';
 
 @Module({
   imports: [ AuthModule, UserModule, JwtModule.register({
     secret: 'my-chat-api',
     signOptions: { expiresIn: '1h' },
-  }), GroupModule,],
+  }), GroupModule, MessageModule,],
   controllers: [AppController],
   providers: [AppService, JwtAuthService, ChatGateway],
 })
@@ -22,6 +23,6 @@ export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
     consumer
       .apply(UserMiddleware)
-      .forRoutes('users', 'group', "users/add-friend");
+      .forRoutes('users', 'group', "users/add-friend", "message");
   }
 }
